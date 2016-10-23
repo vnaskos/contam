5f0b2e6524ca0bf698272c63a90fcad6dcfec0c6
Vavouraz <xarisvavouras@gmail.com>
2016-10-23 19:29:08 +0300

    Find IP Location, display X-Y axis

---
+++ Contam/src/main/java/com/bugbusters/contam/location/FindMyLocation.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bugbusters.contam.location;
+
+import java.io.File;
+import java.io.IOException;
+import com.maxmind.geoip.Location;
+import com.maxmind.geoip.LookupService;
+import com.maxmind.geoip.regionName;
+
+/**
+ *
+ * @author Vavouraz
+ */
+public class FindMyLocation {
+    
+    public static void main(String[] args) {
+	FindMyLocation obj = new FindMyLocation();
+	ServerLocation location = obj.getLocation("2.85.191.243"); //put your IP
+	System.out.println(location);
+  }
+
+  public ServerLocation getLocation(String ipAddress) {
+
+	File file = new File(
+	    "C:\\Users\\Vavouraz\\Desktop\\GeoLiteCity.dat"); //put the location of GeoLiteCity.dat file
+	return getLocation(ipAddress, file);
+
+  }
+
+  public ServerLocation getLocation(String ipAddress, File file) {
+
+	ServerLocation serverLocation = null;
+
+	try {
+
+	serverLocation = new ServerLocation();
+
+	LookupService lookup = new LookupService(file,LookupService.GEOIP_MEMORY_CACHE);
+	Location locationServices = lookup.getLocation(ipAddress);
+
+	serverLocation.setCountryCode(locationServices.countryCode);
+	serverLocation.setCountryName(locationServices.countryName);
+	serverLocation.setRegion(locationServices.region);
+	serverLocation.setRegionName(regionName.regionNameByCode(
+             locationServices.countryCode, locationServices.region));
+	serverLocation.setCity(locationServices.city);
+	serverLocation.setPostalCode(locationServices.postalCode);
+	serverLocation.setLatitude(String.valueOf(locationServices.latitude));
+	serverLocation.setLongitude(String.valueOf(locationServices.longitude));
+
+	} catch (IOException e) {
+		System.err.println(e.getMessage());
+	}
+
+	return serverLocation;
+
+  }
+}
---
+++ Contam/src/main/java/com/bugbusters/contam/location/ServerLocation.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bugbusters.contam.location;
+
+/**
+ *
+ * @author Vavouraz
+ */
+public class ServerLocation {
+    private String countryCode;
+	private String countryName;
+	private String region;
+	private String regionName;
+	private String city;
+	private String postalCode;
+	private String latitude;
+	private String longitude;
+
+	@Override
+	public String toString() {
+		return city + " " + postalCode + ", " + regionName + " (" + region
+				+ "), " + countryName + " (" + countryCode + ") " + latitude
+				+ "," + longitude;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+}
+
