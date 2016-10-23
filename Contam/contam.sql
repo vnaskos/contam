2643c65380899ab78e8de8d8c575a5c37d6b9410
Vavouraz <xarisvavouras@gmail.com>
2016-10-23 15:51:51 +0300

    Database

--- Contam/contam.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE business(
-	`id` INT(11) AUTO_INCREMENT NOT NULL,
-	`name` VARCHAR(255),
-	`latitude` DOUBLE,
-	`longitude` DOUBLE,
-	`description` VARCHAR(255),
-	`address` TEXT,
-	PRIMARY KEY (`id`)
-);
