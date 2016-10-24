6b0ef819e72018f2aca53d891f7a467e4179d636
Vasilis Naskos <vnaskos@gmail.com>
2016-10-24 16:20:33 +0300

    Add sample search API for android test

---
+++ Contam/src/main/java/com/bugbusters/contam/controller/api/SampleSearchApiController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bugbusters.contam.controller.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Vasilis Naskos <http://vnaskos.com>
+ */
+@WebServlet(name = "SearchApiController", urlPatterns = {"/api/search"}, initParams = {
+    @WebInitParam(name = "keyword", value = "business"),
+    @WebInitParam(name = "x", value = "0"),
+    @WebInitParam(name = "y", value = "0")})
+public class SampleSearchApiController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.print("{\"id\": \"1\", \"name\": \"Farmakeio\", \"address\": \"Olympiou 47, Serres 621 25, Greece\"}");
+        out.flush();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- Contam/src/main/java/com/bugbusters/contam/location/FindMyLocation.java
+++ Contam/src/main/java/com/bugbusters/contam/location/FindMyLocation.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 import com.maxmind.geoip.Location;
 import com.maxmind.geoip.LookupService;
 import com.maxmind.geoip.regionName;
-import java.io.InputStream;
-import java.util.Properties;
 
 /**
  *
