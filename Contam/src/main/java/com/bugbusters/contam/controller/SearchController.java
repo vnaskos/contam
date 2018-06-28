dc21c0e91bba6cda90e32e31428fc8a50f975f7b
Vasileios Naskos <vasileios.naskos@cern.ch>
2018-06-28 10:49:44 +0200

    Refactor SearchController to spring component

--- Contam/src/main/java/com/bugbusters/contam/controller/SearchController.java
+++ Contam/src/main/java/com/bugbusters/contam/controller/SearchController.java
@@ -1,104 +1,55 @@
 package com.bugbusters.contam.controller;
 
-import com.bugbusters.contam.orm.business.BusinessDTO;
-import com.bugbusters.contam.location.LocationFinder;
 import com.bugbusters.contam.location.Location;
-import java.io.IOException;
+import com.bugbusters.contam.location.LocationFinder;
+import com.bugbusters.contam.orm.business.BusinessDTO;
+import com.bugbusters.contam.orm.business.BusinessRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Vasilis Naskos <http://vnaskos.com>
- */
-@WebServlet(name = "SearchController", urlPatterns = {"/search"}, initParams = {
-    @WebInitParam(name = "keyword", value = "key"),
-    @WebInitParam(name = "x", value = "0.0"),
-    @WebInitParam(name = "y", value = "0.0")})
-public class SearchController extends HttpServlet {
+@Controller
+public class SearchController {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        
-        String keyword = request.getParameter("keyword");
-        double x;
-        double y;
-        
-        if(request.getParameter("x") != null && request.getParameter("y") != null) {
-            x = Double.parseDouble(request.getParameter("x"));
-            y = Double.parseDouble(request.getParameter("y"));
-        } else {
-            LocationFinder locationFinder = new LocationFinder();
-            String clientIP = locationFinder.getClientPublicIP();
-            Location location = locationFinder.getLocation(clientIP);
+    private BusinessRepo businessRepo;
 
-            x = Double.parseDouble(location.getLatitude());
-            y = Double.parseDouble(location.getLongitude());
+    private LocationFinder locationFinder;
+
+    @GetMapping("/search")
+    public String search(Model model,
+                         @RequestParam(value = "keyword", defaultValue = "") String keyword,
+                         @RequestParam(value = "x", required = false) Double x,
+                         @RequestParam(value = "y", required = false) Double y) {
+        Double latitude = x;
+        Double longitude = y;
+
+        if(x == null || y == null) {
+            String publicIp = locationFinder.getClientPublicIP();
+            Location userLocation = locationFinder.getLocation(publicIp);
+
+            latitude = Double.parseDouble(userLocation.getLatitude());
+            longitude = Double.parseDouble(userLocation.getLongitude());
         }
-        
-//        BusinessDAOImpl businessDAO = new BusinessDAOImpl();
-//        List<BusinessDTO> results = businessDAO.searchBusiness(keyword, x, y);
-        
-        request.setAttribute("keyword", keyword);
-        request.setAttribute("latitude", x);
-        request.setAttribute("longitude", y);
-//        request.setAttribute("results", results);
-        
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+        List<BusinessDTO> businesses = businessRepo.find(keyword, latitude, longitude);
+        model.addAttribute("latitude", latitude);
+        model.addAttribute("longitude", longitude);
+        model.addAttribute("results", businesses);
+
+        return "index";
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    @Autowired
+    public void setBusinessRepo(BusinessRepo businessRepo) {
+        this.businessRepo = businessRepo;
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    @Autowired
+    public void setLocationFinder(LocationFinder locationFinder) {
+        this.locationFinder = locationFinder;
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
